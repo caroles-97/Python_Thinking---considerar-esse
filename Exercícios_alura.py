5f8d8ede23f9e4fff56aa8f235264430bea60099
Carol <carol.tiago9793@gmail.com>
2026-03-29 14:01:40 -0300

    If-else

--- Exercícios_alura.py
+++ Exercícios_alura.py
@@ -4,4 +4,16 @@ nome = input("Digite seu nome: ")
 idade = input("Digite sua idade: ")
 
 
-print ("Meu nome é", nome, "e tenho", idade , "anos")
+print (f"Meu nome é {nome} e tenho {idade} anos")
+
+print(" A\n L\n U\n R\n A\n") # ou print('A','L','U','R','A',sep='\n')
+
+print('''Camiseta Unissex
+Tamanho: P, M, G, GG
+Material: 100% algodão
+Cores disponíveis: Preto, Branco, Vermelho''') #ou abaixo
+print("Camiseta Barbie","Tamanho: P, M, G, GG","Material: 100% algodão","Cores disponíveis: Rosa, Branco", sep ='\n')
+
+
+
+
--- alura.py
+++ alura.py
@@ -18,4 +18,15 @@ print('3. Avaliar restaurante')
 print('4. Sair')
 
 option = input('Escolha uma opção: ')
-print('Voce escolheu a opção', option )
+# option = int('Escolha uma opção :') #se quiser transformar a opção em número inteiro, mas nesse caso não é necessário, pois o input já gera string, e as opções estão em string. Se fosse para comparar com números, aí sim seria necessário transformar a opção em número inteiro.
+print(f'Voce escolheu a opção {option}')
+
+if option == '1' :   #se a opção for igual a 1, faça isso
+    print ('Cadastrar restaurante')
+elif option == '2': 
+    print ('Listar restaurantes')
+elif option == '3':
+    print ('Avaliar restaurante')
+else:
+    print ('Encerrando o programa')
+
