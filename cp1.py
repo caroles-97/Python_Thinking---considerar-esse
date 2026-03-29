df2847577311730e386ff6f115b02fd2c205fe58
caroles-97 <carol.tiago9793@gmail.com>
2026-03-29 18:21:22 -0300

    REVISÃO CP1

---
+++ cp1.py
@@ -0,0 +1,35 @@
+#Revisão para CP1 - Operadores aritméticos, if-else, match-case, print, input, variáveis
+
+#Algoritmos sequenciais - exercícios
+'''
+#Exercício 1
+
+nome = input("Digite seu nome: ")
+
+print(f"Olá, {nome}! Seja bem-vinda à revisão para a CP1 de Python!")
+
+#Exercício 2 
+valor1 = float(input("Digite o primeiro valor: "))
+valor2 = float(input("Digite o segundo valor: "))
+soma = valor1 + valor2
+print(f"A soma de {valor1} e {valor2} é {soma}.")
+
+#Exercício 3
+N1 = float(input("Digite sua nota 1: "))
+N2 = float(input("Digite sua nota 2: "))
+N3 = float(input("Digite sua nota 3: "))
+
+media = (N1 + N2 + N3) / 3
+print(f"Sua média é {media}")
+
+#Exercício 4
+C = float(input("Digite a temperatura em Celsius: "))
+F = C * 1.8 +32
+print(f"A temperatura em {C} ºC é equivalente a {F} ºF")
+'''
+
+#Exercício 5
+P = float( input("Diga seu peso em kg: "))
+A = float(input("Diga sua altura em metros: "))
+IMC = P /A**2
+print(f"Seu IMC é {IMC}")
