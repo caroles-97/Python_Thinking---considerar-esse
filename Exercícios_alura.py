ca786a58f6034273bccf0d8a9564b1e55b88bfbd
Carol <carol.tiago9793@gmail.com>
2026-03-29 12:47:27 -0300

    Exercícios alura

---
+++ Exercícios_alura.py
@@ -0,0 +1,7 @@
+print("Python na Escola de Programação da Alura\n")
+      
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+
+print ("Meu nome é", nome, "e tenho", idade , "anos")
