427db62bcaf46b82dc32b3eda0f68202bbbc9669
Carol <carol.tiago9793@gmail.com>
2026-04-06 20:39:11 -0300

    doc:laço de repetição - WHILE

---
+++ LACO_REPETICAO.py
@@ -0,0 +1,41 @@
+cont = 1
+
+# Enquanto "cont" for maior ou igual a 10
+while cont <= 10: 
+    print (cont)
+    cont += 1    #cont == cont +1
+
+print ("Fim")
+_____________________________________
+
+Exercício: solicitar para o usuário um nº e imprimiremos uma tabuada
+
+print ("Esta é a tabuada do número: \n")
+
+n = int (input("Informe um número para ver a tabuada: "))
+
+cont = 0
+while cont <= 10: 
+    print (f" {n} * {cont} = {n*cont}")
+    cont += 1 
+
+print("FIM")
+_________________________________________
+
+Exercício: escreva um algoritmo que solicite 15 números e informe o somatório de todos os números ímpares digitados. 
+
+cont = 1
+
+while cont <= 15: 
+
+    N = int (input (f"Informe o {cont}º número: "))
+    if N%2 != 0:
+    # se o resto da divisão de N por 2 for diferente de 0.
+        total += N
+
+    cont += 1
+print (f" A soma de todos os ímpares é {total} .")
+
+
+
+
