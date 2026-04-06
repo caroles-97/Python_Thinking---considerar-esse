ca2c8c279c93a9b5fabe0f9fbef4316a2dfff0bb
Carol <carol.tiago9793@gmail.com>
2026-04-06 20:10:50 -0300

    doc finalizado

--- CP1_corrigida.py
+++ CP1_corrigida.py
@@ -1,11 +1,13 @@
 print ("CALCULADORA DE TARIFAS\n")
-print ("1. Estudante\n 2. Trabalhador\n 3. Idoso\n 4. Comum\n ")
+print (" 1. Estudante\n 2. Trabalhador\n 3. Idoso\n 4. Comum\n ")
 
 tarifa = 2.5
 erro = "Opção inválida!"
 
 opc = int (input("Escolha uma opção: "))
-km = float(input("Informe a distância percorrida: "))
+
+if opc > 0 and opc < 5:
+    km = float(input("Informe a distância percorrida: "))
 
 match opc:
     case 1 : 
@@ -20,7 +22,7 @@ match opc:
         print(erro)
     #    Para opção inválida, imprimimos a variável 'erro'.  
 
-# Só vou imprimir o total se o opc tiver entre 1 e 4. Qualquer coisa fora disso, não será impresso.
-
-print (f"Sua passagem custa R$ {total:.2f}")
+# Só vou imprimir o total se o opc tiver entre 1 e 4. Qualquer coisa fora disso, não será impresso. Portanto:
+if opc > 0 and opc < 5:
+    print (f"Sua passagem custa R$ {total:.2f}")
 
