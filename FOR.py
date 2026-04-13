029a49543d24ab3d675ddf97b225e3427a9434b1
Carol <carol.tiago9793@gmail.com>
2026-04-13 20:47:54 -0300

    doc: FOR

---
+++ FOR.py
@@ -0,0 +1,45 @@
+# Exigir os n inteiros de 0 a 50 
+# for i in range (51):
+#     print (i)
+
+
+
+# Escreva um algoritmo que exiba todos os número inteiros de 200 a 100 (em ordem decrescente):
+
+# for i in range ((200-1), 99, -2):
+#     print (i)
+
+
+
+# Leia 10 nº inteiros e informe o maior e o menor nº
+# max = 0 
+# min = 99999999 
+
+# for i in range (1, 11): 
+#     n = int(input(f"Informe o {i}º número: "))
+#     if n > max:
+#         max = n
+#     elif n < min:
+#         min = n
+
+# print (f"O maior valor digitado foi {max}\n"
+#        f"O menor valor digitado foi {min}.")
+
+
+
+
+# Solicite o valor e calcule o nº fatorial dele: 
+# n = -1 
+# fat = 1 
+# # Porque é o fatorial entre 0 e 1
+
+# while n < 0: 
+#     n = int (input("Informe o valor para calcular o fatorial: "))
+
+# if n == 0 or n == 1: 
+#     print (f"O fatorial de {n}! = {fat}")
+
+# else: 
+#     for i in range ( n, 1, -1):
+#         fat = fat * i
+#     print (f"O fatorial desse número é {fat}")
