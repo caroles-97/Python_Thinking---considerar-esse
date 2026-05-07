a5581776b7f1e75e0de064bcf1e282aecc09da5e
caroles-97 <carol.tiago9793@gmail.com>
2026-05-06 22:43:44 -0300

    Exercício 6 - da lista complementar de WHILE AULA 12
    Added an algorithm to check if a number is prime, including input validation and different approaches.

--- exercicios_WHILE.py
+++ exercicios_WHILE.py
@@ -37,5 +37,46 @@ print (f"Existem {maioresDeIdade } pessoas maiores de idade.")
 
 
 
+# AULA 12 - LISTA COMPLEMENTAR DE WHILE: 
+
+# Exercício 6: Fazer um algoritmo que solicite um nº e informe se esse nº é primo ou não. Nº primo é aquele que somente é divisível por 1 e por ele mesmo.
+
+N = 0
+
+while N < 2: 
+    # Enquanto N < 2, vou mandar fazer novamente
+    N = int (input("Informe o número para determinar se é primo"))
+
+
+for i in range (2, N):
+    if N%i == 0:
+    # se o resto da divisao de N / i == 0, logo não é primo. Podemos meter um break e falar que não é primo
+        print (f" O número {N} não é primo.")
+        break
+
+print (f"O número {N} é primo.")
+
+# OU OUTRA FORMA DE RESOLVER
+
+N = 0
+
+while N < 2: 
+    # Enquanto N < 2, vou mandar fazer novamente
+    N = int (input("Informe o número para determinar se é primo"))
+
+if N == 2:
+    print (f"O número {N} é primo.")
+
+else: 
+    for i in range (2, N):
+        if N%i == 0:
+    # se o resto da divisao de N / i == 0, logo não é primo. Podemos meter um break e falar que não é primo
+            print (f" O número {N} não é primo.")
+            break
+
+    if i == N - 1:
+        # Se i for igual N - 1:
+        print (f"O número {N} é primo.")
+
 
 
